4196689a8d1785035999bed522dbf9dd0a1ea5c0
Himanshu1044 <swamihimanshu78@gmail.com>
2026-02-23 13:37:24 +0530

    Data Preprocessing commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# importing data
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values
+y = dataset.iloc[:,-1].values
+
+# for missing data we use fit method 
+missing_data = dataset.isnull().sum()
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy='mean')
+imputer.fit(X[:,1:3])
+X[:,1:3] = imputer.transform(X[:,1:3])
+
+# Encoding for independent variable(one hot encoding) 
+categorical_features = [0]
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder',OneHotEncoder(),categorical_features)],remainder='passthrough')
+X = (ct.fit_transform(X))
+
+# Encoding for Dependent variable(Label encoding)
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# Train_Test_split(test_size = 20%)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+# Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.fit_transform(X_test)
+
