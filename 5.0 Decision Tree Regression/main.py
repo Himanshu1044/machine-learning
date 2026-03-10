f29655989f870520336e78f107216c8a7679a3fd
Himanshu1044 <swamihimanshu78@gmail.com>
2026-03-10 22:18:34 +0530

    Updated Decision Tree regression

--- 5.0 Decision Tree Regression/main.py
+++ 5.0 Decision Tree Regression/main.py
@@ -5,6 +5,8 @@ import pandas as pd
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
+print(X)
+print(y)
 
 from sklearn.tree import DecisionTreeRegressor
 regressor = DecisionTreeRegressor(random_state = 0)
